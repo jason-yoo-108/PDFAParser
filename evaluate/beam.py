fa5b6331b37f491266675540079ba1fe35623e7c
Dylan Yung <YungDexiong@hotmail.com>
2020-04-20 22:22:21 -0700

    dae beam search added

--- evaluate/beam.py
+++ evaluate/beam.py
@@ -1,9 +1,10 @@
 from copy import deepcopy
 import torch
-
+import math
 from const import *
 from infcomp_helper import separate_name
 from pdfa.setup import generate_name_pdfa
+from neural_net.dae.denoiser import SequenceDenoisingModel
 from pdfa.symbol import *
 
 
@@ -48,7 +49,7 @@ class BeamInfo():
             }
         }
         """
-    
+
     def result(self):
         components = {
             TITLE: [],
@@ -59,7 +60,8 @@ class BeamInfo():
         }
         for sym in [TITLE, FIRST, MIDDLE, LAST, SUFFIX]:
             for parse in self.name_parse[sym]:
-                components[sym].append(''.join(list(map(lambda x: PRINTABLE[x], parse))))
+                components[sym].append(
+                    ''.join(list(map(lambda x: PRINTABLE[x], parse))))
         result = {
             'firstname': ' '.join(components[FIRST]),
             'middlename': ' '.join(components[MIDDLE]),
@@ -69,13 +71,16 @@ class BeamInfo():
             'log_prob': self.log_prob
         }
         return result
-    
+
     def copy(self):
-        copied = BeamInfo(self.name_parser, self.pdfa.copy(copy_state_pointer=True), self.name)
+        copied = BeamInfo(self.name_parser, self.pdfa.copy(
+            copy_state_pointer=True), self.name)
         copied.log_prob = self.log_prob.clone()
         copied.encoder_info = self.encoder_info
-        copied.predictor_hidden_state = (self.predictor_hidden_state[0].clone(), self.predictor_hidden_state[1].clone())
-        copied.name_parse = list(self.name_parse) if self.name_parse is not None else None
+        copied.predictor_hidden_state = (
+            self.predictor_hidden_state[0].clone(), self.predictor_hidden_state[1].clone())
+        copied.name_parse = list(
+            self.name_parse) if self.name_parse is not None else None
         copied.guide_format = self.guide_format
         copied.pdfa_symbols = list(self.pdfa_symbols)
         return copied
@@ -86,21 +91,22 @@ class BeamManager():
         self.beams = beams
         self.beam_width = beam_width
         self.tmp = []
-    
+
     def queue(self, candidate_beam):
         # Queue potential candidate beams
         self.tmp.append(candidate_beam)
-    
+
     def select_top_k(self):
         # Only retain top k beams
-        self.beams = sorted(self.tmp, key=lambda b: -b.log_prob)[:self.beam_width]
+        self.beams = sorted(self.tmp, key=lambda b: -
+                            b.log_prob)[:self.beam_width]
         self.tmp = []
-    
+
     def name_parse(self):
         # Parses the input name in all beams
         for b in self.beams:
             b.name_parse = separate_name(b.pdfa_symbols[1:], b.name)
-    
+
     def result(self):
         beam_results = [b.result() for b in self.beams]
         return beam_results
@@ -125,19 +131,89 @@ def beam_search(name_parser, full_name, beam_width):
     ...
     """
     with torch.no_grad():
-        initial_beam = BeamInfo(name_parser=name_parser, pdfa=generate_name_pdfa(), name=full_name)
+        initial_beam = BeamInfo(name_parser=name_parser,
+                                pdfa=generate_name_pdfa(), name=full_name)
         bm = BeamManager([initial_beam], beam_width)
         beam_search_pdfa(bm)
         bm.name_parse()
-    # beam_search_denoise(bm)
+        parsed_names_lst = bm.result()
+
+        for entry in parsed_names_lst:
+            firsts = entry['firstname'].split()
+            middles = entry['middlename'].split()
+            lasts = entry['lastname'].split()
+            
+
     return bm.result()
+
+
+def dae_beam_search(dae: SequenceDenoisingModel, names: list):
+    for name in names:
+        length_tensor = torch.LongTensor([len(name)]).to(DEVICE)
+        input_tensor = torch.LongTensor(
+            [dae.input.index(c) for c in name]).to(DEVICE)
+        _, hidden = dae.encode(input_tensor)
+        top_k = top_k_beam_search(dae, length_tensor, hidden)
+
+
+def top_k_beam_search(dae: SequenceDenoisingModel, length_tensor: torch.Tensor, hidden: torch.Tensor, k: int = 6, penalty: float = 4.0):
+    input = torch.LongTensor([dae.input.index(SOS)]).to(DEVICE)
+    output, hidden = dae.decode(input, length_tensor, hidden)
+    probs = output.reshape(dae.output_sz)
+    probs[EOS_idx] = 0
+    top_k_probs, top_k_idx = torch.topk(probs, k, dim=0)
+    
+    output_chars = dae.output
+    EOS_idx = output_chars.index(EOS)
+
+    top_k = []
+    prev_chars = []
+    for i in range(len(top_k_idx)):
+        prev_char = output_chars[top_k_idx[i].item()]
+
+        if i == 0:
+            top_k.append(
+                ([prev_char], -math.log(top_k_probs[i].item()) + penalty, hidden))
+        else:
+            top_k.append(
+                ([prev_char], -math.log(top_k_probs[i].item()), hidden))
+
+        prev_chars.append(prev_char)
+
+    while EOS not in prev_chars:
+        prev_chars = []
+        hypotheses = []
+
+        for name, score, hidden in top_k:
+            prev_char = name[-1]
+            input = torch.LongTensor([dae.input.index(prev_char)]).to(DEVICE)
+            output, hidden = dae.decode(input, length_tensor, hidden)
+            probs = output.reshape(dae.output_sz)
+            top_k_probs, top_k_idx = torch.topk(probs, k, dim=0)
+            top_k_probs = top_k_probs.reshape(k)
+            top_k_idx = top_k_idx.reshape(k)
+
+            for i in range(len(top_k_probs)):
+                current_char = output_chars[top_k_idx[i]]
+                current_prob = top_k_probs[i]
+                name_copy = name.copy()
+                name_copy.append(current_char)
+                new_score = score + -math.log(current_prob)
+                hypotheses.append((name_copy, new_score, hidden))
+
+        hypotheses.sort(key=lambda x: x[1])
+        top_k = hypotheses[:k]
+        top_k[0] = top_k[0][0], top_k[0][1] + penalty, top_k[0][2]
+        prev_chars = [name[-1] for name, probs, hidden in top_k]
+
+    return top_k
 
 
 def beam_search_pdfa(bm):
     """
     Beam Search Through PDFA
     """
-    for b in bm.beams: 
+    for b in bm.beams:
         b.encoder_info = b.guide_format.encode(b.name)
         b.predictor_hidden_state = b.guide_format.init_predictor_hidden()
         b.pdfa_symbols.append(SOS_FORMAT)
@@ -145,11 +221,15 @@ def beam_search_pdfa(bm):
     for i in range(MAX_STRING_LEN):
         for b in bm.beams:
             if not b.pdfa.at_absorbing_state():
-                emission_probs, hidden_state = b.guide_format.predict(b.pdfa_symbols[-1], b.encoder_info[0][i], b.predictor_hidden_state)
-                emission_probs = emission_probs * torch.Tensor(b.pdfa.get_valid_emission_mask()).to(DEVICE)
-                corrected_emission_probs = emission_probs / torch.sum(emission_probs)
+                emission_probs, hidden_state = b.guide_format.predict(
+                    b.pdfa_symbols[-1], b.encoder_info[0][i], b.predictor_hidden_state)
+                emission_probs = emission_probs * \
+                    torch.Tensor(b.pdfa.get_valid_emission_mask()).to(DEVICE)
+                corrected_emission_probs = emission_probs / \
+                    torch.sum(emission_probs)
                 for p_index, p in enumerate(corrected_emission_probs.squeeze()):
-                    if p == 0: continue
+                    if p == 0:
+                        continue
                     # Consider also checking whether the cumulative probability is less than least likely beam
                     # in BeamManager and reject if that is the case
                     candidate_beam = b.copy()
